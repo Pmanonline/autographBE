28b898bffe7ecca40f83762ff2efe3f9a7c1969f
pmanonline <petersonzoconis@gmail.com>
2024-11-30 14:32:56 +0100

    updated server url

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ app.use(visitTrackerMiddleware);
 
 // CORS Configuration
 const corsOptions = {
-  origin: ["*", "http://localhost:5173"],
+  origin: ["*", "http://localhost:5173", "https://autographbackend.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
